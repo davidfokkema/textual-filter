16b6829c53150a73df2ac41b5d74ba6534268beb
David Fokkema <davidfokkema@icloud.com>
2024-07-21 17:58:22 +0200

    Show selected option

--- src/textual_filter/demo.py
+++ src/textual_filter/demo.py
@@ -1,7 +1,7 @@
 from faker import Faker
 from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import Input
+from textual.widgets import Input, Label
 
 from textual_filter.filtered_option_list import FilteredOptionList
 
@@ -12,17 +12,22 @@ class DemoApp(App[None]):
     def compose(self) -> ComposeResult:
         yield Input(placeholder="Type here to filter results...")
         yield FilteredOptionList()
+        yield Label()
 
     def on_mount(self) -> None:
         fake = Faker()
-        Faker.seed(0)
+        Faker.seed(14)
         self.query_one(FilteredOptionList).add_options(
-            [fake.sentence() for _ in range(100)]
+            [fake.sentence() for _ in range(10_000)]
         )
 
     @on(Input.Changed)
     def filter_options(self, event: Input.Changed) -> None:
         self.query_one(FilteredOptionList).filter(event.value, show_score=True)
+
+    @on(FilteredOptionList.OptionSelected)
+    def show_option(self, event: FilteredOptionList.OptionSelected) -> None:
+        self.query_one(Label).update(event.option.prompt)
 
 
 app = DemoApp
